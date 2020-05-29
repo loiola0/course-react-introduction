afff00e8e415342e8739a4bd2f5f92ae9ee24961
loiola0 <victorloiola@alu.ufc.br>
2020-05-28 23:25:25 -0300

    quase finalizando o módulo 2

---
+++ integrando-webpack/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default App = () =>{
+    return(
+        <div>
+            <p>Digital Innovation One</p>
+            <p>Bem Vindo a nossa aula!</p>
+        </div>
+    );
+}
+
---
+++ integrando-webpack/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    //onde o webpack vai pegar os arquivos para montar a árvore
+    //mode:"production",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'bundler.js'
+    },
+    module:{
+        rules:[
+            {
+            test:/\.(js|jsx)$/,
+            exclude:/node_modules/,
+            use:{
+                loader:"babel-loader"
+            }
+        }
+    ]
+  },
+
+
+  plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html"
+        })
+    ]
+
+
+}
+    
